a629b9683f1eac5c7904c2cc7e5401e73fc26476
Daniel Walton <dwalton76@gmail.com>
2017-06-19 21:27:18 -0400

    Added 6x6x6 test cases

--- test/test.py
+++ test/test.py
@@ -80,6 +80,8 @@ if __name__ == '__main__':
         ('4x4x4 random 04',    'test-data/4x4x4-random-04.txt'),
         ('4x4x4 random 05',    'test-data/4x4x4-random-05.txt'),
         ('5x5x5 random 01',    'test-data/5x5x5-random-01.txt'),
+        ('6x6x6 random 03',    'test-data/6x6x6-random-03.txt'),
+        ('6x6x6 random 04',    'test-data/6x6x6-random-04.txt'),
         ('6x6x6 solved 01',    'test-data/6x6x6-solved-01.txt'),
         ('6x6x6 solved 02',    'test-data/6x6x6-solved-02.txt'),
     )
