f13ac7a7c360c2274a7296616d678c05676f00b1
Daniel Walton <dwalton76@gmail.com>
2019-02-17 12:10:01 -0800

    Move test-data directory

--- test.py
+++ test.py
@@ -66,13 +66,13 @@ if __name__ == '__main__':
     # - in the log output grab the "RGB json" and save that in a file in test-data
     # - in the log output grab the "Final cube for kociema", this is what you put in the entry in the test_cases tuple
     test_cases = (
-        ('2x2x2 random 01',    'test/test-data/2x2x2-random-01.txt'),
-        ('3x3x3 random 01',    'test/test-data/3x3x3-random-01.txt'),
-        ('3x3x3 random 02',    'test/test-data/3x3x3-random-02.txt'),
-        ('3x3x3 random 03',    'test/test-data/3x3x3-random-03.txt'),
-        ('4x4x4 random 01',    'test/test-data/4x4x4-random-01.txt'),
-        ('4x4x4 random 02',    'test/test-data/4x4x4-random-02.txt'),
-        ('5x5x5 random 01',    'test/test-data/5x5x5-random-01.txt'),
+        ('2x2x2 random 01',    'test-data/2x2x2-random-01.txt'),
+        ('3x3x3 random 01',    'test-data/3x3x3-random-01.txt'),
+        ('3x3x3 random 02',    'test-data/3x3x3-random-02.txt'),
+        ('3x3x3 random 03',    'test-data/3x3x3-random-03.txt'),
+        ('4x4x4 random 01',    'test-data/4x4x4-random-01.txt'),
+        ('4x4x4 random 02',    'test-data/4x4x4-random-02.txt'),
+        ('5x5x5 random 01',    'test-data/5x5x5-random-01.txt'),
     )
 
     results = []
