bdf09164e2b7147076eac29abb858607edb3250e
Daniel Walton <dwalton76@gmail.com>
2019-03-02 10:13:01 -0500

    Added test-data/5x5x5-random-02/

--- test.py
+++ test.py
@@ -72,6 +72,7 @@ if __name__ == '__main__':
         ('3x3x3 random 01',    'test-data/3x3x3-random-01.txt'),
         ('4x4x4 random 01',    'test-data/4x4x4-random-01.txt'),
         ('5x5x5 random 01',    'test-data/5x5x5-random-01.txt'),
+        ('5x5x5 random 02',    'test-data/5x5x5-random-02.txt'),
         ('7x7x7 random 01',    'test-data/7x7x7-random-01.txt'),
     )
 
